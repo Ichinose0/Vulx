5dfed43346964283eb847aeb6395bf5a9be795bb
Ichinose0 <Ichnsegraph@outlook.com>
2024-01-16 19:08:36 +0900

    Implemented RenderTarget

---
+++ src/target/mod.rs
@@ -0,0 +1,182 @@
+mod png;
+use ash::vk::{CommandBufferBeginInfo, CommandPool, Fence, ImageLayout, SubmitInfo};
+pub use png::*;
+
+use crate::{
+    FrameBuffer, Image, Instance, LogicalDevice, PhysicalDevice, Pipeline, Queue, RenderPass,
+};
+
+pub struct RenderTargetBuilder {
+    buffer: Option<CommandBuffer>,
+    device: Option<LogicalDevice>,
+    physical_device: Option<PhysicalDevice>,
+    instance: Option<Instance>,
+    queue: Option<Queue>,
+    frame_buffer: Option<FrameBuffer>,
+    renderpass: Option<RenderPass>,
+    pipeline: Option<Pipeline>,
+    image: Option<Image>,
+}
+
+impl RenderTargetBuilder {
+    pub fn new() -> Self {
+        Self::default()
+    }
+
+    pub fn instance(mut self, instance: Instance) -> Self {
+        self.instance = Some(instance);
+        self
+    }
+
+    pub fn queue(mut self, queue: Queue) -> Self {
+        self.queue = Some(queue);
+        self
+    }
+
+    pub fn renderpass(mut self, renderpass: RenderPass) -> Self {
+        self.renderpass = Some(renderpass);
+        self
+    }
+
+    pub fn frame_buffer(mut self, frame_buffer: FrameBuffer) -> Self {
+        self.frame_buffer = Some(frame_buffer);
+        self
+    }
+
+    pub fn pipeline(mut self, pipeline: Pipeline) -> Self {
+        self.pipeline = Some(pipeline);
+        self
+    }
+
+    pub fn logical_device(mut self, device: LogicalDevice) -> Self {
+        self.device = Some(device);
+        self
+    }
+
+    pub fn physical_device(mut self, physical_device: PhysicalDevice) -> Self {
+        self.physical_device = Some(physical_device);
+        self
+    }
+
+    pub fn command_buffer(mut self, command_buffer: CommandBuffer) -> Self {
+        self.buffer = Some(command_buffer);
+        self
+    }
+
+    pub fn image(mut self, image: Option<Image>) -> Self {
+        self.image = image;
+        self
+    }
+
+    pub fn build_png(self, file_path: &str) -> Result<PngRenderTarget, ()> {
+        let buffer = match self.buffer {
+            Some(b) => b,
+            None => return Err(()),
+        };
+        let physical_device = match self.physical_device {
+            Some(b) => b,
+            None => return Err(()),
+        };
+        let device = match self.device {
+            Some(b) => b,
+            None => return Err(()),
+        };
+        let instance = match self.instance {
+            Some(b) => b,
+            None => return Err(()),
+        };
+        let queue = match self.queue {
+            Some(b) => b,
+            None => return Err(()),
+        };
+        let frame_buffer = match self.frame_buffer {
+            Some(b) => b,
+            None => return Err(()),
+        };
+        let renderpass = match self.renderpass {
+            Some(b) => b,
+            None => return Err(()),
+        };
+        let pipeline = match self.pipeline {
+            Some(b) => b,
+            None => return Err(()),
+        };
+        Ok(PngRenderTarget {
+            instance,
+            buffer,
+            logical_device: device,
+            physical_device,
+            queue,
+            frame_buffer,
+            render_pass: renderpass,
+            pipeline,
+            image: self.image,
+            path: file_path.to_owned(),
+        })
+    }
+}
+
+impl Default for RenderTargetBuilder {
+    fn default() -> Self {
+        Self {
+            buffer: None,
+            device: None,
+            physical_device: None,
+            instance: None,
+            queue: None,
+            frame_buffer: None,
+            renderpass: None,
+            pipeline: None,
+            image: None,
+        }
+    }
+}
+
+pub struct CommandBuffer {
+    command_pool: CommandPool,
+    cmd_buffers: Vec<ash::vk::CommandBuffer>,
+}
+
+impl CommandBuffer {
+    pub fn new(device: &LogicalDevice, queue_family_index: usize) -> Self {
+        let command_pool = device.create_command_pool(queue_family_index);
+        let cmd_buffers = device.allocate_command_buffer(command_pool);
+        Self {
+            command_pool,
+            cmd_buffers,
+        }
+    }
+
+    pub(crate) fn begin(&self, device: &LogicalDevice) {
+        unsafe {
+            let begin_info = CommandBufferBeginInfo::builder().build();
+            device
+                .inner
+                .begin_command_buffer(self.cmd_buffers[0], &begin_info)
+                .unwrap();
+        }
+    }
+
+    pub(crate) fn end(&self, device: &LogicalDevice) {
+        unsafe {
+            device
+                .inner
+                .end_command_buffer(self.cmd_buffers[0])
+                .unwrap();
+        }
+    }
+
+    pub(crate) fn submit(&self, device: &LogicalDevice, queue: Queue) {
+        let submit_cmd_buf = vec![self.cmd_buffers[0]];
+        let info = vec![SubmitInfo::builder()
+            .command_buffers(&submit_cmd_buf)
+            .build()];
+        unsafe {
+            device
+                .inner
+                .queue_submit(queue.0, &info, Fence::null())
+                .unwrap();
+            device.inner.queue_wait_idle(queue.0).unwrap();
+        }
+    }
+}
---
+++ src/target/png.rs
@@ -0,0 +1,117 @@
+use std::{fs::File, io::BufWriter};
+
+use ash::vk::{
+    ClearValue, Extent2D, MemoryAllocateInfo, MemoryMapFlags, MemoryPropertyFlags, Offset2D,
+    PipelineBindPoint, Rect2D, RenderPassBeginInfo, SubpassContents,
+};
+
+use super::CommandBuffer;
+
+use crate::{
+    FrameBuffer, Image, Instance, IntoPath, LogicalDevice, PhysicalDevice, Pipeline, Queue,
+    RenderPass, RenderTarget,
+};
+
+pub struct PngRenderTarget {
+    pub(crate) buffer: CommandBuffer,
+    pub(crate) instance: Instance,
+    pub(crate) logical_device: LogicalDevice,
+    pub(crate) physical_device: PhysicalDevice,
+    pub(crate) queue: Queue,
+
+    pub(crate) frame_buffer: FrameBuffer,
+    pub(crate) render_pass: RenderPass,
+    pub(crate) pipeline: Pipeline,
+
+    pub(crate) image: Option<Image>,
+    pub(crate) path: String,
+}
+
+impl PngRenderTarget {}
+
+impl RenderTarget for PngRenderTarget {
+    fn begin(&self) {
+        self.buffer.begin(&self.logical_device);
+        unsafe {
+            let mut clear = ClearValue::default();
+            clear.color.float32[0] = 1.0;
+            clear.color.float32[1] = 1.0;
+            clear.color.float32[2] = 1.0;
+            clear.color.float32[3] = 1.0;
+            let create_info = RenderPassBeginInfo::builder()
+                .render_pass(self.render_pass.inner)
+                .framebuffer(self.frame_buffer.inner)
+                .render_area(
+                    Rect2D::builder()
+                        .extent(
+                            Extent2D::builder()
+                                .width(self.image.as_ref().unwrap().viewport.width as u32)
+                                .height(self.image.as_ref().unwrap().viewport.height as u32)
+                                .build(),
+                        )
+                        .offset(Offset2D::builder().x(0).y(0).build())
+                        .build(),
+                )
+                .clear_values(&[clear])
+                .build();
+            self.logical_device.inner.cmd_begin_render_pass(
+                self.buffer.cmd_buffers[0],
+                &create_info,
+                SubpassContents::INLINE,
+            );
+        }
+    }
+
+    fn fill<P>(&self, path: P, color: crate::Color, thickness: f64)
+    where
+        P: IntoPath,
+    {
+    }
+
+    fn stroke<P>(&self, path: P, color: crate::Color, thickness: f64)
+    where
+        P: IntoPath,
+    {
+    }
+
+    fn end(&self) {
+        unsafe {
+            self.logical_device
+                .inner
+                .cmd_end_render_pass(self.buffer.cmd_buffers[0]);
+            self.logical_device.inner.cmd_bind_pipeline(
+                self.buffer.cmd_buffers[0],
+                PipelineBindPoint::GRAPHICS,
+                self.pipeline.inner,
+            );
+            self.logical_device
+                .inner
+                .cmd_draw(self.buffer.cmd_buffers[0], 3, 1, 0, 0);
+        }
+        self.buffer.end(&self.logical_device);
+        self.buffer.submit(&self.logical_device, self.queue);
+        let file = File::create(&self.path).unwrap();
+        let w = &mut BufWriter::new(file);
+
+        let mut encoder = png::Encoder::new(w, 640, 480);
+        encoder.set_color(png::ColorType::Rgba);
+        encoder.set_depth(png::BitDepth::Eight);
+
+        let mut writer = encoder.write_header().unwrap();
+
+        let data = self.image.unwrap().map_memory(&self.logical_device);
+
+        let slice: &[u8] = unsafe { std::slice::from_raw_parts(data as *const u8, 1228800) };
+        writer.write_image_data(&slice).unwrap();
+        unsafe {
+            self.logical_device
+                .inner
+                .queue_wait_idle(self.queue.0)
+                .unwrap();
+        }
+    }
+
+    fn set_image(&mut self, image: crate::Image) {
+        self.image = Some(image);
+    }
+}
