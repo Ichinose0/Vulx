50d5fd64c25b1991bf07621b801a45576cd2e84e
Ichinose0 <148521661+Ichinose0@users.noreply.github.com>
2024-01-18 12:21:04 +0900

    Added test

--- src/lib.rs
+++ src/lib.rs
@@ -21,17 +21,42 @@ pub use renderpass::*;
 pub use shader::*;
 pub use types::*;
 
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
-
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn geometry_size() {
+        use vulx::geometry::PathGeometry;
+        const VERTEX_SIZE: usize = 6;
+        let mut triangle = PathGeometry::new();
+    triangle.triangle(
+        Vec3::new(
+            Vec4::new(0.0, -0.5, 0.0, 1.0),
+            Vec4::new(0.5, 0.5, 0.0, 1.0),
+            Vec4::new(-0.5, 0.5, 0.0, 1.0),
+        ),
+        Vec3::new(
+            Vec4::new(1.0, 0.0, 0.0, 1.0),
+            Vec4::new(0.0, 1.0, 0.0, 1.0),
+            Vec4::new(0.0, 0.0, 1.0, 1.0),
+        ),
+    );
+
+    triangle.rectangle(
+        Vec4::new(
+            Vec4::new(-0.5, -0.5, 0.0, 1.0),
+            Vec4::new(0.5, -0.5, 0.0, 1.0),
+            Vec4::new(0.5, 0.5, 0.0, 1.0),
+            Vec4::new(-0.5, 0.5, 0.0, 1.0),
+        ),
+        Vec4::new(
+            Vec4::new(1.0, 0.0, 0.0, 1.0),
+            Vec4::new(0.0, 1.0, 0.0, 1.0),
+            Vec4::new(0.0, 0.0, 1.0, 1.0),
+            Vec4::new(1.0, 1.0, 0.0, 1.0),
+        ),
+    );
+    assert_eq!(triangle.size(),VERTEX_SIZE);
     }
 }
